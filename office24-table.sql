8dbb79bef553e09a40316d959af670547f815ec1
mmm55555 <mayfifthfive555@gmail.com>
2024-08-07 17:48:28 +0900

    20240807 a MemberOffice 하는중

--- office24-table.sql
+++ office24-table.sql
@@ -11,42 +11,43 @@ create sequence wish_seq start with 1 increment by 1 nocache nocycle;
 create sequence booking_seq start with 1 increment by 1 nocache nocycle;
 
 -------------------------------------------table-----------------------------------------
-
+drop table member;
 create table member (
-    no       number default member_seq.nextval primary key,             -- 구분 코드
-    id       varchar2(12) check ( length(id) >= 6 ) not null unique,    -- 아이디 (6자 이상 12자 이하 한글x)
-    pw       varchar2(100) not null,           							-- 비밀번호 (8자 이상 16자 이하 한글x, 영문 대문자, 소문자, 숫자, 특수문자 각 1개씩 포함)
-    name     varchar2(12) check ( length(name) >= 2 ) not null,         -- 이름 (2자 이상 12자 이하 영어x)
-    phone    char(14) not null,                                         -- 번호 (번호, 이메일 둘 중 하나만 필수)
-    email    varchar2(32) default null,                                 -- 이메일 (번호, 이메일 둘 중 하나만 필수)
-    birth    date not null,                                             -- 생일
-    gender   varchar2(1) default null,                                  -- 성별 ('M', 'W' // 선택)
-    reg_date date default systimestamp                                  -- 가입일
+                        no       number default member_seq.nextval primary key,             -- 구분 코드
+                        id       varchar2(30) check ( length(id) >= 6 ) not null unique,    -- 아이디 (6자 이상 12자 이하 한글x)
+                        pw       varchar2(100) not null,           							-- 비밀번호 (8자 이상 16자 이하 한글x, 영문 대문자, 소문자, 숫자, 특수문자 각 1개씩 포함)
+                        name     nvarchar2(30) check ( length(name) >= 2 ) not null,         -- 이름 (2자 이상 12자 이하 영어x)
+                        phone    char(11) not null,                                         -- 번호 (번호, 이메일 둘 중 하나만 필수)
+                        email    varchar2(32) default null,                                 -- 이메일 (번호, 이메일 둘 중 하나만 필수)
+                        birth    date not null,                                             -- 생일
+                        gender   varchar2(1) default null,                                  -- 성별 ('M', 'W' // 선택)
+                        reg_date date default systimestamp                                  -- 가입일
 );
 
 -----------------------------------------------------------------------------------------
 
 create table manager (
-    no       number default manager_seq.nextval primary key,            -- 구분 코드
-    id       varchar2(12) check ( length(id) >= 6 ) not null unique,    -- 오피스 관리자 아이디 (6자 이상 12자 이하 한글x)
-    pw       varchar2(100) not null,   								    -- 오피스 관리자 비밀번호 (8자 이상 16자 이하 한글x, 영문 대문자, 소문자, 숫자, 특수문자 각 1개씩 포함)
-    name     varchar2(12) check ( length(name) >= 2 ) not null,         -- 오피스 관리자 이름 (2자 이상 12자 이하 영어x)
-    phone    char(11) not null,                                         -- 오피스 관리자 번호 (번호, 이메일 둘 중 하나만 필수)
-    email    varchar2(32) default null,                                 -- 오피스 관리자 이메일 (번호, 이메일 둘 중 하나만 필수)
-    reg_date date default systimestamp                                  -- 가입일
+                         no       number default manager_seq.nextval primary key,            -- 구분 코드
+                         id       varchar2(12) check ( length(id) >= 6 ) not null unique,    -- 오피스 관리자 아이디 (6자 이상 12자 이하 한글x)
+                         pw       varchar2(100) not null,   								    -- 오피스 관리자 비밀번호 (8자 이상 16자 이하 한글x, 영문 대문자, 소문자, 숫자, 특수문자 각 1개씩 포함)
+                         name     varchar2(12) check ( length(name) >= 2 ) not null,         -- 오피스 관리자 이름 (2자 이상 12자 이하 영어x)
+                         phone    char(11) not null,                                         -- 오피스 관리자 번호 (번호, 이메일 둘 중 하나만 필수)
+                         email    varchar2(32) default null,                                 -- 오피스 관리자 이메일 (번호, 이메일 둘 중 하나만 필수)
+                         reg_date date default systimestamp                                  -- 가입일
 );
 
 -----------------------------------------------------------------------------------------
 
 create table admin (
-    no       number default admin_seq.nextval primary key,              -- 구분 코드
-    id       varchar2(12) not null unique,  	                        -- 총 관리자 아이디 (6자 이상 12자 이하 한글x)
-    pw       varchar2(100) not null,         							-- 총 관리자 비밀번호 (8자 이상 16자 이하 한글x, 영문 대문자, 소문자, 숫자, 특수문자 각 1개씩 포함)
-    lv       number(1) not null,                                      	-- 총 관리자 권한 레벨
-    reg_date date default systimestamp                                	-- 가입일
+                       no       number default admin_seq.nextval primary key,              -- 구분 코드
+                       id       varchar2(12) not null unique,  	                        -- 총 관리자 아이디 (6자 이상 12자 이하 한글x)
+                       pw       varchar2(100) not null,         							-- 총 관리자 비밀번호 (8자 이상 16자 이하 한글x, 영문 대문자, 소문자, 숫자, 특수문자 각 1개씩 포함)
+                       lv       number(1) not null,                                      	-- 총 관리자 권한 레벨
+                       reg_date date default systimestamp                                	-- 가입일
 );
 
 -----------------------------------------------------------------------------------------
+drop table office;
 create table office (
                         no           number default office_seq.nextval primary key,
                         manager_no   number,
@@ -67,51 +68,51 @@ create table office (
 
 
 -----------------------------------------------------------------------------------------
-
+drop table office_image;
 create table office_image (
-    no        number default office_image_seq.nextval primary key,      -- 구분 코드
-    office_no number
-        references office ( no )
-            on delete cascade,					                        -- 오피스 번호
-    name      char(41) not null                                         -- 사진 이름
+                              no        number default office_image_seq.nextval primary key,      -- 구분 코드
+                              office_no number
+                                  references office ( no )
+                                      on delete cascade,					                        -- 오피스 번호
+                              name      char(41) not null                                         -- 사진 이름
 );
 
 -----------------------------------------------------------------------------------------
 
 create table notice (
-    no         number default notice_seq.nextval primary key,           -- 구분 코드
-    title      varchar2(100) not null,                                  -- 제목
-    content    varchar2(4000) not null,                                 -- 내용
-    view_count number not null,            		                        -- 조회 수
-    reg_date   date default systimestamp                                -- 생성일
+                        no         number default notice_seq.nextval primary key,           -- 구분 코드
+                        title      varchar2(100) not null,                                  -- 제목
+                        content    varchar2(4000) not null,                                 -- 내용
+                        view_count number not null,            		                        -- 조회 수
+                        reg_date   date default systimestamp                                -- 생성일
 );
 
 -----------------------------------------------------------------------------------------
-
+drop table review;
 create table review (
-    no        number default review_seq.nextval primary key,            -- 구분 코드
-    member_no number												    -- 작성자
-        references member ( no )
-            on delete cascade,
-    office_no number												    -- 오피스
-        references office ( no )
-            on delete cascade,
-    rating    number check ( rating between 0 and 5 ) not null, 	    -- 점수
-    content   varchar2(4000) not null,         						    -- 내용
-    reg_date  date default systimestamp        						    -- 생성일
+                        no        number default review_seq.nextval primary key,            -- 구분 코드
+                        member_no number												    -- 작성자
+                            references member ( no )
+                                on delete cascade,
+                        office_no number												    -- 오피스
+                            references office ( no )
+                                on delete cascade,
+                        rating    number check ( rating between 0 and 5 ) not null, 	    -- 점수
+                        content   varchar2(4000) not null,         						    -- 내용
+                        reg_date  date default systimestamp        						    -- 생성일
 );
 
 -----------------------------------------------------------------------------------------
-
+drop table wish;
 create table wish (
-    no        number default wish_seq.nextval primary key,              -- 구분 코드
-    member_no number
-        references member ( no )
-            on delete cascade,               	                        -- 작성자
-    office_no number
-        references office ( no )
-            on delete cascade,               	                        -- 오피스
-    reg_date  date default systimestamp                                 -- 생성일
+                      no        number default wish_seq.nextval primary key,              -- 구분 코드
+                      member_no number
+                          references member ( no )
+                              on delete cascade,               	                        -- 작성자
+                      office_no number
+                          references office ( no )
+                              on delete cascade,               	                        -- 오피스
+                      reg_date  date default systimestamp                                 -- 생성일
 );
 
 -----------------------------------------------------------------------------------------
@@ -121,8 +122,7 @@ drop table payment;
 create table payment (
                          no         number default payment_seq.nextval primary key,          -- 구분 코드
                          office_no  number,					                        -- 오피스 번호
-                         member_no  number
-,					                        -- 멤버
+                         member_no  number,                  -- 멤버
                          guests number not null,			                        -- 예약자
                          payment_method    nvarchar2(20) not null,			                        -- 결제 수단
                          start_date date not null,               	                        -- 예약 시작일
@@ -131,47 +131,3 @@ create table payment (
                          reg_date   date default systimestamp, 		                        -- 예약일
                          constraint chk_date check ( start_date <= end_date )                -- 예약 조건
 );
-
-
-
-
-
-
-
-
-
-
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price, reg_date)
-VALUES (101, 45, 6, 'Credit Card', TO_DATE('2024-08-01', 'YYYY-MM-DD'), TO_DATE('2024-08-05', 'YYYY-MM-DD'), 500000, DEFAULT);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price, reg_date)
-VALUES (102, 45, 6, 'PayPal', TO_DATE('2024-08-10', 'YYYY-MM-DD'), TO_DATE('2024-08-12', 'YYYY-MM-DD'), 300000, DEFAULT);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price, reg_date)
-VALUES (103, 45, 6, 'Bank Transfer', TO_DATE('2024-08-15', 'YYYY-MM-DD'), TO_DATE('2024-08-18', 'YYYY-MM-DD'), 700000, DEFAULT);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price, reg_date)
-VALUES (104, 45, 6, 'Credit Card', TO_DATE('2024-09-01', 'YYYY-MM-DD'), TO_DATE('2024-09-03', 'YYYY-MM-DD'), 400000, DEFAULT);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price, reg_date)
-VALUES (105, 45, 6, 'Credit Card', TO_DATE('2024-09-05', 'YYYY-MM-DD'), TO_DATE('2024-09-10', 'YYYY-MM-DD'), 800000, DEFAULT);
-commit;
-
-
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price)
-VALUES (201, 46, 2, 'Credit Card', TO_DATE('2024-08-01', 'YYYY-MM-DD'), TO_DATE('2024-08-03', 'YYYY-MM-DD'), 200000);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price)
-VALUES (202, 46, 1, 'PayPal', TO_DATE('2024-09-10', 'YYYY-MM-DD'), TO_DATE('2024-09-12', 'YYYY-MM-DD'), 150000);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price)
-VALUES (203, 46, 3, 'Bank Transfer', TO_DATE('2024-10-05', 'YYYY-MM-DD'), TO_DATE('2024-10-07', 'YYYY-MM-DD'), 300000);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price)
-VALUES (204, 46, 2, 'Credit Card', TO_DATE('2024-11-15', 'YYYY-MM-DD'), TO_DATE('2024-11-18', 'YYYY-MM-DD'), 250000);
-
-INSERT INTO payment (office_no, member_no, guests, payment_method, start_date, end_date, price)
-VALUES (205, 46, 4, 'Credit Card', TO_DATE('2024-12-01', 'YYYY-MM-DD'), TO_DATE('2024-12-05', 'YYYY-MM-DD'), 400000);
-commit;
